5ea80f84984d88838618bb0e054cfee4182ef487
Michal Krenek (Mikos) <m.krenek@gmail.com>
2015-04-17 14:42:43 +0200

    Update version to 1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='ReParser',
-      version='1.0',
+      version='1.1',
       description='Simple regex-based lexer/parser for inline markup',
       author='Michal Krenek (Mikos)',
       author_email='m.krenek@gmail.com',
